1cb1fe56b2dbbdd6770dd030a3d58f2ce583129e
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-01-14 13:53:11 +0300

    home

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Home/home.css";
+const Home = () => {
+  return (
+    <div className="category">
+      {/* <img src="https://images.pexels.com/photos/18585128/pexels-photo-18585128/free-photo-of-the-path.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" />
+      <img src="https://images.pexels.com/photos/18585128/pexels-photo-18585128/free-photo-of-the-path.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" />
+      <img src="https://images.pexels.com/photos/18585128/pexels-photo-18585128/free-photo-of-the-path.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" /> */}
+    </div>
+  );
+};
+
+export default Home;
